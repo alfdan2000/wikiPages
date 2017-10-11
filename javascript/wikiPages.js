6bfd1228820cb77c2bd0119e8615ec8303ebf0f0
Alfred Cazares <alfred.cazares@gmail.com>
2017-10-11 16:59:05 -0600

    first commit

---
+++ javascript/wikiPages.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    $("#searchButton").click(function() {
+        console.log("hello");
+        $('#searchingInput').css("visibility", "visible");
+        $('#searchingimage').css("visibility", "hidden");
+    });
+});
