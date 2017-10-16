ab63da73452dcc0658e7ec0e3722cd123d5d5912
Alfred Cazares <alfred.cazares@gmail.com>
2017-10-16 16:10:04 -0600

    added search feature

--- javascript/wikiPages.js
+++ javascript/wikiPages.js
@@ -1,8 +1,47 @@
 $(document).ready(function () {
 
-    $("#searchButton").click(function() {
-        console.log("hello");
+    $("#searchButton").click(function () {
         $('#searchingInput').css("visibility", "visible");
         $('#searchingimage').css("visibility", "hidden");
+        
     });
 });
+
+function buttonPressed() {
+    if (event.keyCode == 13) {
+        searchItem();
+    }
+
+}
+
+function searchItem() {
+    $('.results').css("visibility", "visible");
+    $('#resultDescr').css("visibility", "visible");
+    var item = $('#inputbox').val().toString();
+    console.log(item);
+    
+
+    var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + item + "&format=json&callback=?";
+    
+    $.getJSON(wikiUrl, function (data) {
+        //$('#results').empty();
+        for (var x = 0; data[1][x] != null; x++) {
+            jQuery('<div/>', {
+                "id": x,
+                "class": " w3-row"
+            }).appendTo('#results');
+
+            
+            var name = document.createElement('a');
+            var description = document.createElement('p');
+            name.innerHTML = data[1][x];
+            name.className = "w3-col m4 l3 results"
+            description.innerHTML = data[2][x];
+            description.className = "w3-col m8 l6 results"
+            
+            document.getElementById(x).appendChild(name);
+            document.getElementById(x).appendChild(description);
+        }
+    })
+}
+
